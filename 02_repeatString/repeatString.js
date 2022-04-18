a59ab42b3e91720828056a24a14195bd64077c57
Darkstar1712 <dylanmcc1992@gmail.com>
2022-04-17 20:10:52 -0700

    Completed Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    let string = "";
 
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < numberOfTimes; i++) {
+        string += stringToRepeat;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (const element of elementsToRemove) {
+        array.splice(array.indexOf(element), 1);
+    }  
+    return array;
 };
 
 // Do not edit below this line
